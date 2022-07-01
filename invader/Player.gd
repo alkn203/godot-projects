d7f5780bf1c63be705dba234effd24042ec28f53
alkn203 <alkn203@gmail.com>
2022-07-02 01:52:57 +0900

    update

--- invader/Player.gd
+++ invader/Player.gd
@@ -5,6 +5,7 @@ extends Area2D
 var speed = 100
 onready var shot_scene = preload("res://Shot.tscn")
 onready var shot_layer = get_node("/root/Main/ShotLayer")
+onready var shot_timer = get_node("ShotTimer") 
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,8 +21,10 @@ func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		# Move as long as the key/button is pressed.
 		position.x -= speed * delta
-	if Input.is_action_just_pressed("space_key"):
+	if Input.is_action_just_pressed("space_key") and shot_timer.is_stopped():
 		# shoot
 		var shot = shot_scene.instance()
 		shot.position = position
 		shot_layer.add_child(shot)
+		#
+		shot_timer.start()
