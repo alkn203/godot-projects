b5518cc31f620a505f734a9914c3141d591016e5
alkn203 <alkn203@gmail.com>
2022-07-21 23:57:46 +0900

    update

---
+++ invader/Beam.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+var speed = 50 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y += speed * delta
+
+
+func _on_Beam_area_entered(area):
+	# If target is player
+	if area.name == "Player":
+		get_tree().paused = true
+		yield(get_tree().create_timer(1.0), "timeout")
+		print(2)
--- invader/Enemy.gd
+++ invader/Enemy.gd
@@ -4,8 +4,11 @@ extends Area2D
 # Declare member variables here. Examples:
 var speed = 10
 var move_direction = -1
-var can_move_down = false;
+var can_move_down = false
 const DOWN_DISTANCE = 32
+const PROB_BEAM = 0.0002
+onready var beam_scene = preload("res://Beam.tscn")
+onready var beam_layer = get_node("/root/Main/BeamLayer")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +19,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += move_direction * speed * delta
+	#
 	if can_move_down:
 		position.y += DOWN_DISTANCE
 		can_move_down = false
+	#
+	if randf() < PROB_BEAM:
+		var beam = beam_scene.instance()
+		beam.position = position
+		beam_layer.add_child(beam)
