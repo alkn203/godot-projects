d3261b816eba110884f2935cd6f1c87849cc95a4
alkn203 <alkn203@gmail.com>
2022-08-08 00:29:28 +0900

    update

--- sokoban/Player.gd
+++ sokoban/Player.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 
 # Declare member variables here. Examples:
-var grid_pos = Vector2(4, 8)
+var tile_pos = Vector2(4, 8)
 onready var tilemap = get_node("/root/Main/Stage1/TileMap")
 onready var baggage_layer = get_node("/root/Main/Stage1/BaggageLayer")
 
@@ -30,18 +30,31 @@ func _process(delta):
 		v.y = 1
 	#
 	if v.x != 0 or v.y != 0:
-		var next = grid_pos + v
+		var next = tile_pos + v
 		#print(tilemap.get_cellv(next))
 		if tilemap.get_cellv(next) == TILE_WALL:
 			return
-		
-		for baggage in baggage_layer.get_children():
-			#
-			if baggage.tile_pos == next:
-				if tilemap.get_cellv(next + v) == TILE_WALL:
-					return
+		# 隣が荷物かどうか調べる
+		var baggage = _get_baggage_by_pos(next)
+		# 荷物の場合
+		if baggage != null:
+			# 荷物のその１つ先が壁
+			if tilemap.get_cellv(next + v) == TILE_WALL:
+				return
+			# 荷物のその１つ先が荷物
+			if _get_baggage_by_pos(next + v) != null:
+				return
+			# 荷物位置更新
+			baggage.tile_pos = next + v
+			baggage.position += v * TILE_SIZE
 		# プレイヤー位置更新
-		grid_pos = next
+		tile_pos = next
 		position += v * TILE_SIZE
 
-# 指定のインデックス位置にある荷物を返す
+
+# 指定位置の荷物を返す
+func _get_baggage_by_pos(pos):
+	for baggage in baggage_layer.get_children():
+		if baggage.tile_pos == pos:
+			return baggage
+	return null
