2ec612742abe5d04e15bfeb3be8b187d80f5cca1
alkn203 <alkn203@gmail.com>
2022-08-26 23:32:40 +0900

    Update MyPanel.gd

--- minesweeper/MyPanel.gd
+++ minesweeper/MyPanel.gd
@@ -1,9 +1,10 @@
 extends Area2D
+class_name MyPanel
 
 
 # Declare member variables here.
-var is_bomb = false
-var is_open = false
+var is_bomb: bool = false
+var is_open: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +13,8 @@ func _ready():
 
 
 func _on_Panel_input_event(viewport, event, shape_idx):
-	# マウスボタンの入力イベント
+	# マウス入力イベントが発生 
 	if event is InputEventMouseButton: 
+		# マウスボタン押下イベント 
 		if event.is_pressed(): 
 			get_parent().open_panel(self)
