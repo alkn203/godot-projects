e0b8f2daac4876547afc1de6589b2e4574ade985
alkn203 <alkn203@gmail.com>
2022-08-28 17:35:00 +0900

    update

--- breakout/Paddle.gd
+++ breakout/Paddle.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 var sprite_size = get_node("Sprite").texture.get_size()
+var dragging = false
+
 onready var screen_size = get_viewport_rect().size
 
 
@@ -11,11 +13,23 @@ func _ready():
 	pass # Replace with function body.
 
 
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			# ドラッグフラグ
+			dragging = true
+		else:
+			dragging = false
+	if event is InputEventMouseMotion:
+		if dragging:
+			# マウス位置取得
+			var pos = event.position
+			# パドルの横座標を追従させ、画面からはみ出ないようにする
+			var pos_min = sprite_size.x / 2
+			var pos_max = screen_size.x - sprite_size.x / 2
+			position.x = clamp(pos.x, pos_min, pos_max)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# マウス位置取得
-	var pos = get_viewport().get_mouse_position()
-	# パドルの横座標を追従させ、画面からはみ出ないようにする
-	var pos_min = sprite_size.x / 2
-	var pos_max = screen_size.x - sprite_size.x / 2
-	position.x = clamp(pos.x, pos_min, pos_max)
+	pass
