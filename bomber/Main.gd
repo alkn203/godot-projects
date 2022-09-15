848f3945c2dd49f74bd02b87bcaab1b4e71db74e
alkn203 <alkn203@gmail.com>
2022-09-15 10:21:37 +0900

    Update Main.gd

--- bomber/Main.gd
+++ bomber/Main.gd
@@ -1,1 +1,23 @@
+extends Node2D
 
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var TILE_SIZE = 64
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func locate_object(obj, pos):
+        obj.tile_pos = pos
+        obj.position.x = pos.x * TILE_SIZE
+        obj.position.y = pos.y * TILE_SIZE
