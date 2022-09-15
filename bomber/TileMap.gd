97e40a6a59b769fad83944a91c53b9f35668e0be
alkn203 <alkn203@gmail.com>
2022-09-15 10:50:02 +0900

    Update TileMap.gd

--- bomber/TileMap.gd
+++ bomber/TileMap.gd
@@ -14,13 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func global_to_map(pos):
-	var local_pos = to_local(pos)
-	var map_pos = world_to_map(local_pos)
-	return map_pos
-
-func map_to_global(pos):
-	var local_pos = map_to_world(pos)
-	var global_pos = to_global(local_pos)
-	return global_pos
