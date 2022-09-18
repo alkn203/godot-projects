88943521e36f9f9bcb830433feb3299d96a83948
alkn203 <alkn203@gmail.com>
2022-09-18 11:17:05 +0900

    Update Bomb.gd

--- bomber/Bomb.gd
+++ bomber/Bomb.gd
@@ -1,20 +1,14 @@
 extends StaticBody2D
 
-# タイルマップでの位置
-var tile_pos = Vector2(0, 0)
 # 爆弾の威力
 export (int) var power = 1
-
+# タイルマップでの位置
+var tile_pos = Vector2(0, 0)
+# ノード
 onready var main = get_node("/root/Main")
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# 3秒後に爆発
 	yield(get_tree().create_timer(3.0), "timeout")
 	main.explode(self)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
