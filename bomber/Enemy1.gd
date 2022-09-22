9b6166d4476f8737a06bd9ec8dc82caa07aaf3c7
alkn203 <alkn203@gmail.com>
2022-09-22 14:46:31 +0900

    Update Enemy1.gd

--- bomber/Enemy1.gd
+++ bomber/Enemy1.gd
@@ -5,10 +5,18 @@ export (int) var speed = 120
 # 移動方向ベクトル
 var direction = Vector2(-1, 0)
 # ノード
+onready var sprite = get_node("Sprite")
 onready var enemy_layer = get_node("/root/Main/EnemyLayer")
 
 # 毎フレーム処理
+
 func _physics_process(delta):
+        #
+        if direction.x < 0:
+                sprete.frame = 0
+        else:
+                sprite.frame = 1
+
 	var velocity = direction * speed
         # 移動と当たり判定
 	var collision = move_and_collide(velocity * delta)
