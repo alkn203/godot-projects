c8e1aae16eda6133ab15afc530b010b7550e928d
alkn203 <alkn203@gmail.com>
2022-09-22 14:07:37 +0900

    Update Enemy1.gd

--- bomber/Enemy1.gd
+++ bomber/Enemy1.gd
@@ -3,16 +3,18 @@ extends KinematicBody2D
 # 敵の速度
 export (int) var speed = 120
 # 移動方向ベクトル
-var vec = Vector2(-1, 0)
+var direction = Vector2(-1, 0)
 # ノード
 onready var enemy_layer = get_node("/root/Main/EnemyLayer")
 
 # 毎フレーム処理
 func _physics_process(delta):
-	var velocity = vec * speed
+	var velocity = direction * speed
         # 移動と当たり判定
 	var collision = move_and_collide(velocity * delta)
         #
         if collision:
+                if collision.collider is TileMap:
+                        direction = direction.bounce(collision.normal)
                 
 
