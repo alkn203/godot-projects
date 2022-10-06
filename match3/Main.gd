88e21428d176c09adf8b3aacfd875c7ab232c3e1
alkn203 <alkn203@gmail.com>
2022-10-06 15:21:57 +0900

    Update Main.gd

--- match3/Main.gd
+++ match3/Main.gd
@@ -27,7 +27,7 @@ func _ready():
 #	pass
 
 # ジェム初期化
-func init_gem:
+func init_gem():
     # 3つ並び以上があれば仕切り直し
     if exist_match3():
       # シード値変更
@@ -44,7 +44,7 @@ func init_gem:
     init_hidden_gem()
 
 # 画面外のジェム配置
-func init_hidden_gem:
+func init_hidden_gem():
     # 一旦消す
     for gem in gem_layer.get_child():
       if gem.position.y < 0:
@@ -62,4 +62,54 @@ func init_hidden_gem:
 # ペア選択処理
 func select_pair(gem):
 	print(gem.name)
-	
+
+# 3つ並び以上存在チェック
+func _exist_match3():
+    for gem in gem_layer.get_child():
+      # 画面に見えているジェムのみ
+      if gem.y > 0:
+        # 横方向
+        _check_horizontal(gem)
+        _set_mark()
+        # 縦方向
+        _check_vertical(gem)
+        _set_mark();
+
+    for gem in gem_layer.get_child():
+      # 削除対象があれば
+      if gem.mark == "rmv":
+        return true
+    
+    return false
+
+# 横方向の3つ並び以上チェック
+func _check_horizontal(current):
+    if current.mark != "rmv":
+      current.mark = "tmp"
+
+    match_count += 1
+    var next = _get_gem(current.position + Vector2(GRID_SIZE, 0))
+    if (next and current.num == next.num:
+      _check_horizontal(next)
+
+# 縦方向の3つ並び以上チェック
+func _check_vertical(current):
+    if current.mark != "rmv":
+      current.mark = "tmp"
+
+    match_count += 1
+    var next = _get_gem(current.position + Vector2(0, GRID_SIZE))
+    if (next and current.num == next.num:
+      _check_vertical(next)
+
+# マークセット
+func setMark():
+    for gem in gem_layer.get_child():
+      if gem.mark == "tmp":
+        # 3つ並び以上なら削除マーク
+        if match_count > 2:
+          gem.mark = "rmv"
+        else:
+          gem.mark = "normal"
+    
+    match_count = 0
