542c3bebb740dd34b78b6832c2ac5009e64560fc
alkn203 <alkn203@gmail.com>
2022-10-07 00:52:04 +0900

    update

--- match3/Gem.gd
+++ match3/Gem.gd
@@ -10,13 +10,11 @@ onready var main = get_node("/root/Main")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	num = 0
-        mark = "normal"
-
+	mark = "normal"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 
 func _on_Gem_input_event(viewport, event, shape_idx):
 	# マウス入力イベントが発生
--- match3/Main.gd
+++ match3/Main.gd
@@ -9,6 +9,7 @@ const GEM_NUM_X = 8
 # 変数
 # ペア格納用配列
 var pair = []
+var match_count = 0
 # シーン
 const Gem = preload("res://Gem.tscn")
 # レイヤー
@@ -16,56 +17,59 @@ onready var gem_layer = get_node("GemLayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-        # ジェム配置
+	# ジェム配置
 	for i in GEM_NUM_X:
 		for j in GEM_NUM_X:
 			var gem = Gem.instance()
 			gem.position = Vector2(i * GEM_SIZE, j * GEM_SIZE) + Vector2(GEM_OFFSET, GEM_OFFSET)
 			gem_layer.add_child(gem)
-
+	
+	init_gem()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 # ジェム初期化
 func init_gem():
-    # 3つ並び以上があれば仕切り直し
-    if exist_match3():
-      # シード値変更
-      randomize()
-      # 作り直し
-      for gem in gem_layer.get_child():
-        # ランダムな色
-        var num = randi() % 7
-        gem.get_node("Sprite").frame = num
-        gem.num = num
-      
-      init_gem()
-
-    # 画面外ジェム配置
-    init_hidden_gem()
+	# 3つ並び以上があれば仕切り直し
+	if _exist_match3():
+		print("match3")
+		# シード値変更
+		randomize()
+		# 作り直し
+		for gem in gem_layer.get_children():
+			# ランダムな色
+			var num = randi() % 7
+			gem.get_node("Sprite").frame = num
+			gem.num = num
+			gem.mark = "normal"
+			
+		init_gem()
+	# 画面外ジェム配置
+	#init_hidden_gem()
 
 # 画面外のジェム配置
 func init_hidden_gem():
-    # 一旦消す
-    for gem in gem_layer.get_child():
-      if gem.position.y < 0:
-        gem.queue_free()
-
-    # シード値変更
-    randomize()
-    # ジェム配置
-    for i in GEM_NUM_X:
-	for j in GEM_NUM_X:
-		var gem = Gem.instance()
-		gem.position = Vector2(i * GEM_SIZE, j * GEM_SIZE) + Vector2(GEM_OFFSET, GEM_OFFSET)
-                # 画面分上にずらす
-                gem.position -= Vector2(0, SCREEN_HEIGHT)
-		gem_layer.add_child(gem)
-                # ランダムな色
-                var num = randi() % 7
-                gem.get_node("Sprite").frame = num
-                gem.num = num
+	# 一旦消す
+	for gem in gem_layer.get_children():
+		if gem.position.y < 0:
+			gem.queue_free()
+			
+	# シード値変更
+	randomize()
+	# ジェム配置
+	for i in GEM_NUM_X:
+		for j in GEM_NUM_X:
+			var gem = Gem.instance()
+			gem.position = Vector2(i * GEM_SIZE, j * GEM_SIZE) + Vector2(GEM_OFFSET, GEM_OFFSET)
+			# 画面分上にずらす
+			gem.position -= Vector2(0, SCREEN_HEIGHT)
+			gem_layer.add_child(gem)
+			# ランダムな色
+			var num = randi() % 7
+			gem.get_node("Sprite").frame = num
+			gem.num = num
 
 # ペア選択処理
 func select_pair(gem):
@@ -73,51 +77,59 @@ func select_pair(gem):
 
 # 3つ並び以上存在チェック
 func _exist_match3():
-    for gem in gem_layer.get_child():
-      # 画面に見えているジェムのみ
-      if gem.y > 0:
-        # 横方向
-        _check_horizontal(gem)
-        _set_mark()
-        # 縦方向
-        _check_vertical(gem)
-        _set_mark();
-
-    for gem in gem_layer.get_child():
-      # 削除対象があれば
-      if gem.mark == "rmv":
-        return true
-    
-    return false
+	for gem in gem_layer.get_children():
+		# 画面に見えているジェムのみ
+		if gem.position.y > 0:
+			# 横方向
+			_check_horizontal(gem)
+			_set_mark()
+			# 縦方向
+			_check_vertical(gem)
+			_set_mark();
+	
+	for gem in gem_layer.get_children():
+		# 削除対象があれば
+		if gem.mark == "rmv":
+			return true
+	
+	return false
 
 # 横方向の3つ並び以上チェック
 func _check_horizontal(current):
-    if current.mark != "rmv":
-      current.mark = "tmp"
-
-    match_count += 1
-    var next = _get_gem(current.position + Vector2(GRID_SIZE, 0))
-    if (next and current.num == next.num:
-      _check_horizontal(next)
+	if current.mark != "rmv":
+		current.mark = "tmp"
+		
+	match_count += 1
+	var next = _get_gem(current.position + Vector2(GEM_SIZE, 0))
+	if (next != null) and (current.num == next.num):
+		_check_horizontal(next)
 
 # 縦方向の3つ並び以上チェック
 func _check_vertical(current):
-    if current.mark != "rmv":
-      current.mark = "tmp"
-
-    match_count += 1
-    var next = _get_gem(current.position + Vector2(0, GRID_SIZE))
-    if (next and current.num == next.num:
-      _check_vertical(next)
+	if current.mark != "rmv":
+		current.mark = "tmp"
+		
+	match_count += 1
+	var next = _get_gem(current.position + Vector2(0, GEM_SIZE))
+	if (next != null) and (current.num == next.num):
+		_check_vertical(next)
 
 # マークセット
-func setMark():
-    for gem in gem_layer.get_child():
-      if gem.mark == "tmp":
-        # 3つ並び以上なら削除マーク
-        if match_count > 2:
-          gem.mark = "rmv"
-        else:
-          gem.mark = "normal"
-    
-    match_count = 0
+func _set_mark():
+	for gem in gem_layer.get_children():
+		if gem.mark == "tmp":
+			# 3つ並び以上なら削除マーク
+			if match_count > 2:
+				gem.mark = "rmv"
+			else:
+				gem.mark = "normal"
+	
+	match_count = 0
+
+# 指定された位置のジェムを返す
+func _get_gem(pos):
+	# 該当するジェムがあったらループを抜ける
+	for gem in gem_layer.get_children():
+		if gem.position == pos:
+			return gem
+	return null
