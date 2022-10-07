cceec398e52b170d6787c92dd2020384a657bca4
alkn203 <alkn203@gmail.com>
2022-10-08 00:46:01 +0900

    update

--- match3/Gem.gd
+++ match3/Gem.gd
@@ -9,17 +9,17 @@ onready var main = get_node("/root/Main")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	num = 0
-	mark = "normal"
+  num = 0
+  mark = "normal"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_Gem_input_event(viewport, event, shape_idx):
-	# マウス入力イベントが発生
-	if event is InputEventMouseButton:
-		# マウスボタンの押下イベント
-		if event.is_pressed():
-			# Mainの関数に自身を渡す
-			main.select_pair(self)
+  # マウス入力イベントが発生
+  if event is InputEventMouseButton:
+    # マウスボタンの押下イベント
+    if event.is_pressed():
+      # Mainの関数に自身を渡す
+      main.select_pair(self)
--- match3/Main.gd
+++ match3/Main.gd
@@ -5,6 +5,7 @@ const SCREEN_HEIGHT = 640
 const GEM_SIZE = 80
 const GEM_OFFSET = GEM_SIZE / 2
 const GEM_NUM_X = 8
+const SWAP_DURATIOM = 0.25
 
 # 変数
 var pair = []
@@ -18,72 +19,71 @@ onready var gem_layer = get_node("GemLayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# ジェム配置
-	for i in GEM_NUM_X:
-		for j in GEM_NUM_X:
-			var gem = Gem.instance()
-                        var x = i * GEM_SIZE + GEM_OFFSET
-			var x = j * GEM_SIZE + GEM_OFFSET
-                        gem.position = Vector2(x, y)
-			gem_layer.add_child(gem)
-	
-	init_gem()
-	
+  # ジェム配置
+  for i in GEM_NUM_X:
+    for j in GEM_NUM_X:
+      var gem = Gem.instance()
+      var x = i * GEM_SIZE + GEM_OFFSET
+      var y = j * GEM_SIZE + GEM_OFFSET
+      gem.position = Vector2(x, y)
+      gem_layer.add_child(gem)
+  
+  init_gem()
+  
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 # ジェム初期化
 func init_gem():
-	# 3つ並び以上があれば仕切り直し
-	if _exist_match3():
-		print("match3")
-		# シード値変更
-		randomize()
-		# 作り直し
-		for gem in gem_layer.get_children():
-			# ランダムな色
-			var num = randi() % 7
-			gem.get_node("Sprite").frame = num
-			gem.num = num
-			gem.mark = "normal"
-			
-		init_gem()
-	# 画面外ジェム配置
-	#init_hidden_gem()
+  # 3つ並び以上があれば仕切り直し
+  if _exist_match3():
+    # シード値変更
+    randomize()
+    # 作り直し
+    for gem in gem_layer.get_children():
+      # ランダムな色
+      var num = randi() % 7
+      gem.get_node("Sprite").frame = num
+      gem.num = num
+      gem.mark = "normal"
+      
+    init_gem()
+  # 画面外ジェム配置
+  #init_hidden_gem()
 
 # 画面外のジェム配置
 func init_hidden_gem():
-	# 一旦消す
-	for gem in gem_layer.get_children():
-		if gem.position.y < 0:
-			gem.queue_free()
-			
-	# シード値変更
-	randomize()
-	# ジェム配置
-	for i in GEM_NUM_X:
-		for j in GEM_NUM_X:
-			var gem = Gem.instance()
-			var x = i * GEM_SIZE + GEM_OFFSET
-			var x = j * GEM_SIZE + GEM_OFFSET
-                        gem.position = Vector2(x, y)
-                        # 画面分上にずらす
-			gem.position.y -= SCREEN_HEIGHT
-			gem_layer.add_child(gem)
-			# ランダムな色
-			var num = randi() % 7
-			gem.get_node("Sprite").frame = num
-			gem.num = num
-                        gem.mark = "normal"
+  # 一旦消す
+  for gem in gem_layer.get_children():
+    if gem.position.y < 0:
+      gem.queue_free()
+      
+  # シード値変更
+  randomize()
+  # ジェム配置
+  for i in GEM_NUM_X:
+    for j in GEM_NUM_X:
+      var gem = Gem.instance()
+      var x = i * GEM_SIZE + GEM_OFFSET
+      var y = j * GEM_SIZE + GEM_OFFSET
+      gem.position = Vector2(x, y)
+      # 画面分上にずらす
+      gem.position.y -= SCREEN_HEIGHT
+      gem_layer.add_child(gem)
+      # ランダムな色
+      var num = randi() % 7
+      gem.get_node("Sprite").frame = num
+      gem.num = num
+      gem.mark = "normal"
 
 # ペア選択処理
 func select_pair(gem):
   # 一つ目
   if pair.size() == 0:
     # カーソル表示
-    var cursor1 = Cursor().addChildTo(this.cursorGroup);
-    cursor1.setPosition(gem.x, gem.y);
+    #var cursor1 = Cursor().addChildTo(this.cursorGroup);
+    #cursor1.setPosition(gem.x, gem.y);
     pair.append(gem)
     # 隣り合わせ以外を選択不可にする
     _selectable_next()
@@ -91,49 +91,49 @@ func select_pair(gem):
 
   # 二つ目
   if pair.size() == 1:
-      var cursor2 = Cursor().addChildTo(this.cursorGroup);
-      cursor2.setPosition(gem.x, gem.y);
-      pair.append(gem)
-      # 入れ替え処理
-      _swap_gem(false)
+    #var cursor2 = Cursor().addChildTo(this.cursorGroup);
+    #cursor2.setPosition(gem.x, gem.y);
+    pair.append(gem)
+    # 入れ替え処理
+    _swap_gem()
 
 # 隣り合わせ以外を選択不可にする
-_selectable_next():
+func _selectable_next():
     # 一旦全てを選択不可に
     for gem in gem_layer.get_children():
       gem.get_node("CollisionShape2D").set_deferred("disabled", true)
     
     var gem = pair[0]
     for target in gem_layer.get_children():
-      var dx = abs(gem.posotion.x - target.position.x)
+      var dx = abs(gem.position.x - target.position.x)
       var dy = abs(gem.position.y - target.position.y)
       # 上下左右隣り合わせだけを選択可に
       if gem.position.x == target.position.x and dy == GEM_SIZE:
-        gem.get_node("CollisionShape2D").set_deferred("disabled", false)
+        target.get_node("CollisionShape2D").set_deferred("disabled", false)
       if gem.position.y == target.position.y and dx == GEM_SIZE:
-        gem.get_node("CollisionShape2D").set_deferred("disabled", false)
+        target.get_node("CollisionShape2D").set_deferred("disabled", false)
 
 # ジェム入れ替え処理
-_swap_gem():
-    var g1 = pair[0]
-    var g2 = pair[1]
-    # 1回目
-    if !second_swap:
-      _set_gem_collision_disble(true)
-      #this.cursorGroup.children.clear()
+func _swap_gem():
+  var g1 = pair[0]
+  var g2 = pair[1]
+  # 1回目
+  if !second_swap:
+    _set_gem_collision_disble(true)
+    #this.cursorGroup.children.clear()
 
-    swap_count = 2
-    # 入れ替えアニメーション
-    var tween1 = get_tree().create_tween()
-    tween1.tween_property(g1, "position", g2.position, 2.0)
-    tween1.tween_callback(self, "_after_swap")
+  swap_count = 2
+  # 入れ替えアニメーション
+  var tween1 = get_tree().create_tween()
+  tween1.tween_property(g1, "position", g2.position, SWAP_DURATIOM)
+  tween1.tween_callback(self, "_after_swap")
 
-    var tween2 = get_tree().create_tween()
-    tween2.tween_property(g2, "position", g1.position, 2.0)
-    tween2.tween_callback(self, "_after_swap")
+  var tween2 = get_tree().create_tween()
+  tween2.tween_property(g2, "position", g1.position, SWAP_DURATIOM)
+  tween2.tween_callback(self, "_after_swap")
 
 func _after_swap():
-  #
+  # 確実に入れ替えが行われた後に実行
   swap_count -= 1
   if swap_count > 0:
     return
@@ -141,13 +141,14 @@ func _after_swap():
   # 入れ替え後処理
   if second_swap:
     pair.clear();
-    _set_gem_selectable(true)
+    _set_gem_collision_disble(false)
     second_swap = false
   else:
     # 3つ並びがあれば削除処理へ
     if _exist_match3():
       pair.clear();
-      _remove_gem()
+      print("match3")
+      #_remove_gem()
     else:
       # 戻りの入れ替え
       second_swap = true
@@ -155,59 +156,64 @@ func _after_swap():
 
 # 3つ並び以上存在チェック
 func _exist_match3():
-	for gem in gem_layer.get_children():
-		# 画面に見えているジェムのみ
-		if gem.position.y > 0:
-			# 横方向
-			_check_horizontal(gem)
-			_set_mark()
-			# 縦方向
-			_check_vertical(gem)
-			_set_mark();
-	
-	for gem in gem_layer.get_children():
-		# 削除対象があれば
-		if gem.mark == "rmv":
-			return true
-	
-	return false
+  for gem in gem_layer.get_children():
+    # 画面に見えているジェムのみ
+    if gem.position.y > 0:
+      # 横方向
+      _check_horizontal(gem)
+      _set_mark()
+      # 縦方向
+      _check_vertical(gem)
+      _set_mark();
+  
+  for gem in gem_layer.get_children():
+    # 削除対象があれば
+    if gem.mark == "rmv":
+      return true
+  
+  return false
 
 # 横方向の3つ並び以上チェック
 func _check_horizontal(current):
-	if current.mark != "rmv":
-		current.mark = "tmp"
-		
-	match_count += 1
-	var next = _get_gem(current.position + Vector2(GEM_SIZE, 0))
-	if (next != null) and (current.num == next.num):
-		_check_horizontal(next)
+  if current.mark != "rmv":
+    current.mark = "tmp"
+    
+  match_count += 1
+  var next = _get_gem(current.position + Vector2(GEM_SIZE, 0))
+  if (next != null) and (current.num == next.num):
+    _check_horizontal(next)
 
 # 縦方向の3つ並び以上チェック
 func _check_vertical(current):
-	if current.mark != "rmv":
-		current.mark = "tmp"
-		
-	match_count += 1
-	var next = _get_gem(current.position + Vector2(0, GEM_SIZE))
-	if (next != null) and (current.num == next.num):
-		_check_vertical(next)
+  if current.mark != "rmv":
+    current.mark = "tmp"
+    
+  match_count += 1
+  var next = _get_gem(current.position + Vector2(0, GEM_SIZE))
+  if (next != null) and (current.num == next.num):
+    _check_vertical(next)
 
 # マークセット
 func _set_mark():
-	for gem in gem_layer.get_children():
-		if gem.mark == "tmp":
-			# 3つ並び以上なら削除マーク
-			if match_count > 2:
-				gem.mark = "rmv"
-			else:
-				gem.mark = "normal"
-	
-	match_count = 0
+  for gem in gem_layer.get_children():
+    if gem.mark == "tmp":
+      # 3つ並び以上なら削除マーク
+      if match_count > 2:
+        gem.mark = "rmv"
+      else:
+        gem.mark = "normal"
+  
+  match_count = 0
 
+# Gemの選択可不可を決定
+func _set_gem_collision_disble(b):
+  for gem in gem_layer.get_children():
+    gem.get_node("CollisionShape2D").set_deferred("disabled", b)
+  
 # 指定された位置のジェムを返す
 func _get_gem(pos):
-	# 該当するジェムがあったらループを抜ける
-	for gem in gem_layer.get_children():
-		if gem.position == pos:
-			return gem
-	return null
+  # 該当するジェムがあったらループを抜ける
+  for gem in gem_layer.get_children():
+    if gem.position == pos:
+      return gem
+  return null
