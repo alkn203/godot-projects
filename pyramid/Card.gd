5440a7b28d97acdc8f46b3fe085ab278e7f2860d
alkn203 <alkn203@gmail.com>
2022-10-13 10:59:19 +0900

    Update Card.gd

--- pyramid/Card.gd
+++ pyramid/Card.gd
@@ -1,1 +1,27 @@
+extends Area2D
 
+# 変数
+var num
+var mark
+var drop_count
+# ノード取得
+onready var main = get_node("/root/Main")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  num = 0
+  mark = "normal"
+  drop_count = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Gem_input_event(viewport, event, shape_idx):
+  # マウス入力イベントが発生
+  if event is InputEventMouseButton:
+    # マウスボタンの押下イベント
+    if event.is_pressed():
+      # Mainの関数に自身を渡す
+      main.select_pair(self)
