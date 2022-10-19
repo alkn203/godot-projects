51989dfa10134e08c70b7e83e342e03c54815cf4
alkn203 <alkn203@gmail.com>
2022-10-19 15:39:30 +0900

    Update grid.gd

--- sandbox/grid.gd
+++ sandbox/grid.gd
@@ -3,30 +3,29 @@ extends Node2D
 class_name Grid
 
 # 幅
-var width
+var width: float
 # 列数
-var columns
+var columns: int
 # オフセット値
-var offset
+var offset: float
 # 1ユニットのサイズ
-var unit_width
+var unit_width: float
 
 # コンストラクタ
-func _init: function() {
+func _init(float: w, int: col, int: offset = 0):
   width = w
   columns = col
   offset = off
   unit_width = width / columns
 
 # スパン指定で値を取得(負数もok)
-func span(index):
+func span(float: index): -> float
   return (unit_width * index) + offset
 
 # グリッドの1単位の幅
-func unit():
+func unit(): -> float
   return unit_width
 
 # グリッドの中心からの相対位置で値を取得
-center: function(offset) {
-  var index = offset
-  return (width / 2) + (unit_width * index)
+center(float: offset = 0): -> float
+  return (width / 2) + (unit_width * offset)
