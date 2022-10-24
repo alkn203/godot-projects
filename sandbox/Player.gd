fe5a04bcca77c0955eadcce5fb84c2652ae6676b
alkn203 <alkn203@gmail.com>
2022-10-24 17:55:05 +0900

    Update Player.gd

--- sandbox/Player.gd
+++ sandbox/Player.gd
@@ -1,7 +1,12 @@
 extends KinematicBody2D
 
-# プレイヤーの速度
-export (int) var speed = 150
+# 定数
+# 重力
+const GRAVITY = 9.8 / 18
+# ジャンプ力
+var JUMP_POWER = 150
+
+# 変数
 # 移動方向ベクトル
 var velocity = Vector2(0, 0)
 
@@ -15,7 +20,7 @@ func _ready():
 
 # 毎フレーム処理
 func _physics_process(delta):
-  # 移動と当たり判定
+  # 移動と当たり判定んふ
   var collision = move_and_collide(velocity * delta)
 
   if Input.is_action_just_pressed(dir):
