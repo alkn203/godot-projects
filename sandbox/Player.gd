5e0f1ac4fdbe54360cd25b075eb3ec2f79e3dd8d
alkn203 <alkn203@gmail.com>
2022-10-25 15:44:08 +0900

    update

--- sandbox/Player.gd
+++ sandbox/Player.gd
@@ -2,9 +2,9 @@ extends KinematicBody2D
 
 # 定数
 # 重力
-const GRAVITY = 9.8 / 18
+const GRAVITY = 9.8 / 2 
 # ジャンプ力
-var JUMP_POWER = 150
+var JUMP_POWER = 300
 
 # 変数
 # 移動方向ベクトル
@@ -16,14 +16,14 @@ onready var animated_sprite = get_node("AnimatedSprite")
 # 初期化処理
 func _ready():
   # 最初のアニメーション
-  animated_sprite.play("ui_down_stop")
+  animated_sprite.play()
 
 # 毎フレーム処理
 func _physics_process(delta):
   # 移動と当たり判定んふ
   var collision = move_and_collide(velocity * delta)
 
-  if Input.is_action_just_pressed(dir):
+  if Input.is_action_just_pressed("ui_left_click"):
     velocity = Vector2(0, -JUMP_POWER)
 
   velocity += Vector2(0, GRAVITY)
