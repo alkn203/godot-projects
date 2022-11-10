10254e3b5b073453bf303f616b367d053c5f0f06
alkn203 <alkn203@gmail.com>
2022-11-10 20:08:06 +0900

    Update Piece.gd

--- 15puzzle/Piece.gd
+++ 15puzzle/Piece.gd
@@ -1,25 +1,10 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+# ピースタッチ時処理
 func _on_Piece_input_event(viewport, event, shape_idx):
-	# マウス入力イベントが発生
-	if event is InputEventMouseButton:
-		# マウスボタンの押下イベント
-		if event.is_pressed():
-			# 親の関数を呼び出す
-			get_parent().move_piece(self)
+  # マウス入力イベントが発生
+  if event is InputEventMouseButton:
+    # マウスボタンの押下イベント
+      if event.is_pressed():
+        # 親の関数を呼び出す
+        get_parent().move_piece(self)
