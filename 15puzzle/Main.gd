ed06a8e5b015cd94b40dc55930e407414cdaaee1
alkn203 <alkn203@gmail.com>
2022-11-10 20:23:06 +0900

    Update Main.gd

--- 15puzzle/Main.gd
+++ 15puzzle/Main.gd
@@ -1,33 +1,30 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
+# 定数
+const PIECE_SIZE = 160
+
+# ノード
 onready var blank = get_node("Piece16")
-var PIECE_SIZE = 160;
 
-
-# Called when the node enters the scene tree for the first time.
+# 初期化
 func _ready():
-	for piece in get_children():
-		var index = piece.get_index()
-		piece.get_node("Sprite").frame = index
+  for piece in get_children():
+    # インデックス値をスプライトのフレームにする
+    var index = piece.get_index()
+    piece.get_node("Sprite").frame = index
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+# ピース移動処理
 func move_piece(piece):
-	var p_pos = piece.position
-	var b_pos = blank.position
-	# x, yの座標差の絶対値
-	var dx = abs(p_pos.x - b_pos.x);
-	var dy = abs(p_pos.y - b_pos.y);
-	# 隣り合わせの判定
-	if ((p_pos.x == b_pos.x and dy == PIECE_SIZE) or (p_pos.y == b_pos.y and dx == PIECE_SIZE)):
-		# タッチされたピース位置を一時変数に退避
-		var t_pos = Vector2(p_pos.x, p_pos.y)
-		# ピース入れ替え処理
-		piece.position = b_pos
-		blank.position = t_pos
+  var p_pos = piece.position
+  var b_pos = blank.position
+  # x, yの座標差の絶対値
+  var dx = abs(p_pos.x - b_pos.x);
+  var dy = abs(p_pos.y - b_pos.y);
+  # 隣り合わせの判定
+    if ((p_pos.x == b_pos.x and dy == PIECE_SIZE) or (p_pos.y == b_pos.y and dx == PIECE_SIZE)):
+      # タッチされたピース位置を一時変数に退避
+      var t_pos = Vector2(p_pos.x, p_pos.y)
+      # ピース入れ替え処理
+      piece.position = b_pos
+      blank.position = t_pos
