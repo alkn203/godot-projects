bbcf9deaa3ecb5d212ac5bd881d26f7f99e4f0a5
alkn203 <alkn203@gmail.com>
2022-11-10 23:45:51 +0900

    update

--- 15puzzle/Main.gd
+++ 15puzzle/Main.gd
@@ -23,7 +23,7 @@ func move_piece(piece):
   var dx = abs(p_pos.x - b_pos.x);
   var dy = abs(p_pos.y - b_pos.y);
   # 隣り合わせの判定
-    if ((p_pos.x == b_pos.x and dy == PIECE_SIZE) or (p_pos.y == b_pos.y and dx == PIECE_SIZE)):
+  if ((p_pos.x == b_pos.x and dy == PIECE_SIZE) or (p_pos.y == b_pos.y and dx == PIECE_SIZE)):
       # タッチされたピース位置を一時変数に退避
       var t_pos = Vector2(p_pos.x, p_pos.y)
       # ピース入れ替えアニメーション
@@ -31,5 +31,3 @@ func move_piece(piece):
       tween.set_parallel(true)
       tween.tween_property(piece, "position", b_pos, DURATION)
       tween.tween_property(blank, "position", t_pos, DURATION)
-      tween.set_parallel(false)
-      #tween.tween_callback(self, "_after_swap")
