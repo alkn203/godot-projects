98b373054e0fe14e03f58e9780ae5868a2f6adb4
alkn203 <alkn203@gmail.com>
2022-11-11 23:59:20 +0900

    update

--- sokoban/Player.gd
+++ sokoban/Player.gd
@@ -2,9 +2,11 @@ extends Sprite
 
 # 定数
 const TILE_SIZE = 64
+const DURATION = 0.25
 
 # 変数
 var tile_pos = Vector2(4, 8)
+var can_input = true
 
 # ノード
 onready var stage = get_node("/root/Main/Stage1")
@@ -16,6 +18,10 @@ enum { TILE_NONE, TILE_FLOOR, TILE_GOAL, TILE_WALL, BAGGAGE, BAGGAGE_ON_GOAL }
 
 # 毎フレーム処理
 func _process(delta):
+  # キー入力不可の場合
+  if can_input == false:
+    return
+      
   var v = Vector2.ZERO
   # 方向キーチェック
   if Input.is_action_just_pressed("ui_left"):
@@ -28,6 +34,8 @@ func _process(delta):
     v.y = 1
   # 何かしら入力があれば
   if v.x != 0 or v.y != 0:
+    # tween作成
+    var tween = get_tree().create_tween()
     # その方向の一つ先の位置
     var next = tile_pos + v
     # 壁なら何もしない
@@ -47,13 +55,22 @@ func _process(delta):
       
       # 荷物位置更新
       baggage.tile_pos += v
-      baggage.position += v * TILE_SIZE
+      tween.set_parallel(true)
+      tween.tween_property(baggage, "position", baggage.position + v * TILE_SIZE, DURATION)
       # 荷物がゴールに乗っているかのチェック
-      stage.change_baggage_color_on_goal()
+      #stage.change_baggage_color_on_goal()
       # クリアチェック
-      if stage.is_clear() == true:
-        OS.alert("clear")
+      #if stage.is_clear() == true:
+      #  OS.alert("clear")
       
     # プレイヤー位置更新
     tile_pos = next
-    position += v * TILE_SIZE
+    # 一旦入力不可にする
+    can_input = false
+    # 移動アニメーション
+    tween.tween_property(self, "position", position + v * TILE_SIZE, DURATION)
+    tween.set_parallel(false)
+    tween.tween_callback(self, "_after_move")
+
+func _after_move():
+  can_input = true
