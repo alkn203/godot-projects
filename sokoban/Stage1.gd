54283feeafe6f204364ad173290330eeac1dbe06
alkn203 <alkn203@gmail.com>
2022-11-11 19:29:10 +0900

    Update Stage1.gd

--- sokoban/Stage1.gd
+++ sokoban/Stage1.gd
@@ -20,3 +20,26 @@ func _ready():
     baggage.tile_pos = pos
     baggage.position = pos * TILE_SIZE
     baggage_layer.add_child(baggage)
+
+# 指定位置の荷物を返す
+func _get_baggage_by_pos(pos):
+  for baggage in baggage_layer.get_children():
+    if baggage.tile_pos == pos:
+      return baggage
+  # 荷物がない場合
+  return null
+
+# ゴールに乗っている荷物の色を変える 
+func _change_baggage_color_on_goal():
+  for baggage in baggage_layer.get_children(): 
+    if tilemap.get_cellv(baggage.tile_pos) == TILE_GOAL: 
+      baggage.frame = BAGGAGE_ON_GOAL
+    else:
+      baggage.frame = BAGGAGE
+
+# クリアチェック 
+func _is_clear(): 
+  for baggage in baggage_layer.get_children():
+    if baggage.frame == BAGGAGE:
+      return false
+  return true
