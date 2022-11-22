a6f491d141937b41dce607c24dd6ed493038584d
alkn203 <alkn203@gmail.com>
2022-11-22 12:55:20 +0900

    Update Player.gd

--- basicaction/Player.gd
+++ basicaction/Player.gd
@@ -22,25 +22,32 @@ func _ready():
 
 # 毎フレーム処理
 func _physics_process(delta):
-  
-  #velocity = Vector2(0, 0)
-  
+  #
   if Input.is_action_pressed("ui_left"):
-    velocity.x = -150
-  
+    velocity.x = -speed
+  if Input.is_action_pressed("ui_right"):
+    velocity.x = speed
+
+  #
   if Input.is_action_just_released("ui_left"):
     velocity.x = 0
+  if Input.is_action_just_released("ui_right"):
+    velocity.x = 0
+  
   # 移動と当たり判定
   velocity = move_and_slide(velocity, Vector2(0, -1))
+  
   # 床の上なら
   if is_on_floor():
     # アニメーション変更
     if animated_sprite.animation != "default":
       animated_sprite.play("default")
+
     # 左クリックでジャンプ
     if Input.is_action_just_pressed("ui_left_click"):
       velocity = Vector2(0, -JUMP_POWER)
       # アニメーション変更
       animated_sprite.play("jump")
+
   # 重力を加算
-  velocity += Vector2(0, GRAVITY)
+  velocity.y += GRAVITY
