8867d66ba6e5f8d7ccd115763adc104ad3a843ec
alkn203 <alkn203@gmail.com>
2022-11-30 00:21:41 +0900

    update

--- 15puzzle/Main.gd
+++ 15puzzle/Main.gd
@@ -1,19 +1,18 @@
 extends Node2D
 
-
 # 定数
 const PIECE_SIZE = 160
 const DURATION = 0.25
 
 # ノード
-onready var blank = get_node("Piece16")
+@onready var blank = get_node("Piece16")
 
 # 初期化
 func _ready():
   for piece in get_children():
     # インデックス値をスプライトのフレームにする
     var index = piece.get_index()
-    piece.get_node("Sprite").frame = index
+    piece.get_node("Sprite2D").frame = index
 
 # ピース移動処理
 func move_piece(piece):
@@ -24,10 +23,10 @@ func move_piece(piece):
   var dy = abs(p_pos.y - b_pos.y);
   # 隣り合わせの判定
   if ((p_pos.x == b_pos.x and dy == PIECE_SIZE) or (p_pos.y == b_pos.y and dx == PIECE_SIZE)):
-      # タッチされたピース位置を一時変数に退避
-      var t_pos = Vector2(p_pos.x, p_pos.y)
-      # ピース入れ替えアニメーション
-      var tween = get_tree().create_tween()
-      tween.set_parallel(true)
-      tween.tween_property(piece, "position", b_pos, DURATION)
-      tween.tween_property(blank, "position", t_pos, DURATION)
+    # タッチされたピース位置を一時変数に退避
+    var t_pos = Vector2(p_pos.x, p_pos.y)
+    # ピース入れ替えアニメーション
+    var tween = get_tree().create_tween()
+    tween.set_parallel(true)
+    tween.tween_property(piece, "position", b_pos, DURATION)
+    tween.tween_property(blank, "position", t_pos, DURATION)
--- 15puzzle/Piece.gd
+++ 15puzzle/Piece.gd
@@ -5,6 +5,6 @@ func _on_Piece_input_event(viewport, event, shape_idx):
   # マウス入力イベントが発生
   if event is InputEventMouseButton:
     # マウスボタンの押下イベント
-      if event.is_pressed():
-        # 親の関数を呼び出す
-        get_parent().move_piece(self)
+    if event.is_pressed():
+      # 親の関数を呼び出す
+      get_parent().move_piece(self)
