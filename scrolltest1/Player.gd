3b59b34c9ea6f95a367f06d24020673b259b4492
alkn203 <alkn203@gmail.com>
2022-12-06 17:04:13 +0900

    Update Player.gd

--- scrolltest1/Player.gd
+++ scrolltest1/Player.gd
@@ -1,46 +1,25 @@
 extends KinematicBody2D
 
 # 定数
-# 重力
 const GRAVITY = 9.8 / 2 
-# ジャンプ力
 const JUMP_POWER = 304
-# プレイヤーの速度
-export (int) var speed = 150
+const speed = 150
 
 # 変数
-# 移動方向ベクトル
 var velocity = Vector2.ZERO
 
 # ノード
-onready var animated_sprite = get_node("AnimatedSprite")
+onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 
 # 初期化処理
-func _ready():
+func _ready() -> void:
   # 最初のアニメーション
-  animated_sprite.play("stop_left")
+  animated_sprite.play("walk_right")
   
 # 毎フレーム処理
-func _physics_process(delta):
-  # 左右キー移動
-  if Input.is_action_pressed("ui_left"):
-    velocity.x = -speed
-    # アニメーション変更
-    animated_sprite.play("walk_left")
-  if Input.is_action_pressed("ui_right"):
-    velocity.x = speed
-    animated_sprite.play("walk_right")
-
-  # 左右キーを離した時
-  if Input.is_action_just_released("ui_left"):
-    velocity.x = 0
-    animated_sprite.play("stop_left")
-  if Input.is_action_just_released("ui_right"):
-    velocity.x = 0
-    animated_sprite.play("stop_right")
-  
+func _physics_process(delta) -> void:
   # 移動と当たり判定
-  velocity = move_and_slide(velocity, Vector2(0, -1))
+  velocity = move_and_slide(velocity, Vector2.UP)
   
   # 床の上なら
   if is_on_floor():
