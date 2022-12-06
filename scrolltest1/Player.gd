1af034af3f5b8ec9e2da03a1aa616d2f0f888bfe
alkn203 <alkn203@gmail.com>
2022-12-06 17:53:30 +0900

    Update Player.gd

--- scrolltest1/Player.gd
+++ scrolltest1/Player.gd
@@ -6,15 +6,17 @@ const JUMP_POWER = 304
 const speed = 150
 
 # 変数
-var velocity = Vector2.ZERO
+var velocity: Vector2
 
 # ノード
-onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
+onready var anim: AnimatedSprite = get_node("AnimatedSprite")
 
 # 初期化処理
 func _ready() -> void:
+  # 右に移動
+  velocity = Vector2.RIGHT * speed
   # 最初のアニメーション
-  animated_sprite.play("walk_right")
+  anim.play("walk_right")
   
 # 毎フレーム処理
 func _physics_process(delta) -> void:
@@ -27,5 +29,10 @@ func _physics_process(delta) -> void:
     if Input.is_action_just_pressed("ui_jump"):
       velocity.y = -JUMP_POWER
 
+  # 壁に接触なら
+  if is_on_wall():
+    #
+    print("wall")  
+ 
   # 重力を加算
   velocity.y += GRAVITY
