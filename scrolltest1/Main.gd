a8f85b11057f9faec848a161d40e90f3b50f6561
alkn203 <alkn203@gmail.com>
2022-12-08 16:23:06 +0900

    Update Main.gd

--- scrolltest1/Main.gd
+++ scrolltest1/Main.gd
@@ -8,12 +8,19 @@ extends Node2D
 # ノード
 onready var player = get_node("Player")
 onready var camera = get_node("Camera2D")
+onready var screen_size = get_viewport_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass
+  #
+  var cx = screen_size.x / 2
+  var cy = screen_size.y / 2
+  #
+  camera.position = Vector2(cx, cy)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  camera.position.x = player.position.x - 400
+  #camera.position.x = player.position.x - 400
+  pass
+_
