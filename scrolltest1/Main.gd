b8ea8361b144fa9d09317dfa954bc9e6eabffe74
alkn203 <alkn203@gmail.com>
2022-12-09 13:56:07 +0900

    Update Main.gd

--- scrolltest1/Main.gd
+++ scrolltest1/Main.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 # ノード
+onready var player: Player = get_node("Player")
 onready var camera: Camera2D = get_node("Camera2D")
 
 # 毎フレーム処理
 func _process(delta):
-  #camera.position.x = player.position.x - 400
-  pass
+  # カメラをプレイヤーに追従させる
+  camera.position.x = player.position.x
