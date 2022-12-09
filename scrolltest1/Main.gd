7d2e01d2b6ebcd0b02c4b808ed0022d1c868d57b
alkn203 <alkn203@gmail.com>
2022-12-09 11:48:16 +0900

    update

--- scrolltest1/Main.gd
+++ scrolltest1/Main.gd
@@ -1,26 +1,9 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # ノード
-onready var player = get_node("Player")
-onready var camera = get_node("Camera2D")
-onready var screen_size = get_viewport_rect().size
+onready var camera: Camera2D = get_node("Camera2D")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-  #
-  var cx = screen_size.x / 2
-  var cy = screen_size.y / 2
-  #
-  camera.position = Vector2(cx, cy)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# 毎フレーム処理
 func _process(delta):
   #camera.position.x = player.position.x - 400
   pass
-_
--- scrolltest1/Player.gd
+++ scrolltest1/Player.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+class_name Player
 
 # 定数
 const GRAVITY = 9.8 / 2 
@@ -16,14 +17,14 @@ func _ready():
   # 最初のアニメーション
   anim.play("walk_right")
   # 少し待ってから移動（ワープっぽい動き対策）
-  yield(get_tree().create_timer(0.5), "timeout")
+  yield(get_tree().create_timer(0.3), "timeout")
   # 右に移動
   velocity = Vector2(speed, 0)
   
 # 毎フレーム処理
 func _physics_process(delta):
   # 衝突前のベクトルを退避
-  var prev_velocity = velocity
+  var prev_velocity: Vector2 = velocity
   # 移動と当たり判定
   velocity = move_and_slide(velocity, Vector2.UP)
   # 床の上なら
@@ -36,10 +37,8 @@ func _physics_process(delta):
   if is_on_wall():
     # 衝突情報から反射ベクトルを算出
     if get_slide_count() > 0:
-      var collision = get_slide_collision(0)
+      var collision: KinematicCollision2D = get_slide_collision(0)
       velocity = prev_velocity.bounce(collision.normal)
-      # 画像反転
-      scale.x *= -1
- 
+      
   # 重力を加算
   velocity.y += GRAVITY
