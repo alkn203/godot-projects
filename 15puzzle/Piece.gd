ecf4f393e3155f04228d8a1d574e3dfa8804cf5a
alkn203 <alkn203@gmail.com>
2022-12-26 01:04:46 +0900

    update

--- 15puzzle/Piece.gd
+++ 15puzzle/Piece.gd
@@ -6,6 +6,6 @@ func _on_Piece_input_event(viewport, event, shape_idx) -> void:
   # マウス入力イベント
   if event is InputEventMouseButton:
     # マウスボタンの押下
-      if event.is_pressed():
-        # 親の関数を呼び出して自身を渡す
-        get_parent().move_piece(self)
+    if event.is_pressed():
+      # 親の関数を呼んで自身を渡す
+      get_parent().move_piece(self)
