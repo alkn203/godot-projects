b1c738fff1306c49ff04915d16aff83e196991c9
alkn203 <alkn203@gmail.com>
2023-01-06 14:00:56 +0900

    Update Main.gd

--- blocks/Main.gd
+++ blocks/Main.gd
@@ -20,7 +20,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  pass
+  cur_time += delta
 
 # 落下ブロック作成
 func _create_block() -> void:
