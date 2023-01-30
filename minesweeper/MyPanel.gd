4762493cb16c068d75ecd548fa9634dfeba00d1e
alkn203 <alkn203@gmail.com>
2023-01-30 10:25:34 +0900

    Update MyPanel.gd

--- minesweeper/MyPanel.gd
+++ minesweeper/MyPanel.gd
@@ -1,9 +1,13 @@
-extends Area2D
 class_name MyPanel
+extends Area2D
 
 # 変数
+var index_pos: Vecror2 = Vector2.ZERO
 var is_bomb: bool = false
 var is_open: bool = false
+
+# シーン
+onready var main_scene = get_node("/root/Main")
 
 # パネルへの入力イベント
 func _on_MyPanel_input_event(viewport, event, shape_idx):
@@ -11,4 +15,4 @@ func _on_MyPanel_input_event(viewport, event, shape_idx):
   if event is InputEventMouseButton: 
     # マウスボタン押下 
     if event.is_pressed(): 
-      get_parent().open_panel(self)
+      main_scene.open_panel(self)
