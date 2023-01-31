acfe02e94c078233680568af040e7d6c68b66ce8
alkn203 <alkn203@gmail.com>
2023-01-31 22:30:45 +0900

    update

--- invader/Title.gd
+++ invader/Title.gd
@@ -9,10 +9,10 @@ onready var anim = get_node("AnimationPlayer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.play("fadeout_in")
+  anim.play("fadeout_in")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("space_key"):
-		get_tree().change_scene("res://Main.tscn")
+  if Input.is_action_just_pressed("space_key"):
+    get_tree().change_scene("res://Main.tscn")
