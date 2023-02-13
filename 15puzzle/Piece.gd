4ce89224708dc05ca3fd147a6f39a1b99fca7e91
alkn203 <alkn203@gmail.com>
2023-02-13 18:15:07 +0900

    Update Piece.gd

--- 15puzzle/Piece.gd
+++ 15puzzle/Piece.gd
@@ -1,6 +1,9 @@
 class_name Piece
 extends Area2D
 
+# ノード
+onready const main: Node2D = get_node("/root/Main")
+
 # インデックス位置
 var index_pos: Vector2 = Vector2.ZERO
 
@@ -10,5 +13,5 @@ func _on_Piece_input_event(viewport, event, shape_idx) -> void:
   if event is InputEventMouseButton:
     # マウスボタンの押下
     if event.is_pressed():
-      # 親の関数を呼んで自身を渡す
-      get_parent().move_piece(self)
+      # メインシーンの関数を呼んで自身を渡す
+      main.move_piece(self)
