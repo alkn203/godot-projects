3aa70d85f748f73f6e17944d84c14ce03b71fd2d
alkn203 <alkn203@gmail.com>
2023-02-14 07:52:25 +0900

    Update Piece.gd

--- 15puzzle/Piece.gd
+++ 15puzzle/Piece.gd
@@ -1,11 +1,12 @@
 class_name Piece
 extends Area2D
 
+# インデックス位置
+var index_pos: Vector2 = Vector2.ZERO
+
 # ノード
 onready var main: Node2D = get_node("/root/Main")
 
-# インデックス位置
-var index_pos: Vector2 = Vector2.ZERO
 # タッチ時処理
 func _on_Piece_input_event(_viewport, event, _shape_idx) -> void:
   # マウス入力イベント
