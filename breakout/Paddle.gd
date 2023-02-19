8b1753cb6a2a7f43ca7aa1f9a6c6498085f403c1
alkn203 <alkn203@gmail.com>
2023-02-19 22:57:46 +0900

    update

--- breakout/Paddle.gd
+++ breakout/Paddle.gd
@@ -1,7 +1,11 @@
+class_name Paddle
 extends KinematicBody2D
 
+# 定数
+const X_SENSE = 4
+
 # 変数
-var sprite_size: Vetor2 = get_node("Sprite").texture.get_size()
+var sprite_size: Vector2 = get_node("Sprite").texture.get_size()
 var dragging: bool = false
 var prev_x: float
 var dx: float = 0
@@ -11,9 +15,7 @@ onready var screen_size = get_viewport_rect().size
 
 # 初期化処理
 func _ready() -> void:
-    # 前のx座標
-    prev_ = position.x
-    # 左右移動端
+    # 左右移動の端
     min_x = sprite_size.x / 2
     max_x = screen_size.x - sprite_size.x / 2
 
@@ -23,13 +25,17 @@ func _input(event):
         if event.pressed:
             # ドラッグフラグ
             dragging = true
+            prev_x = event.position.x
         else:
             dragging = false
     if event is InputEventMouseMotion:
         if dragging:
             # マウスx座標取得
-            var pos_x: Vector2 = event.position.x
+            var pos_x: float = event.position.x
             # 前のx座標との差を元に移動量を算出
-            dx = (pos_x - prev_x) * 10 
+            dx = (pos_x - prev_x) * X_SENSE
+            position.x += dx 
             # 画面からはみ出ないようにする
-            position.x = clamp(pos.x, pos_min, pos_max)
+            position.x = clamp(position.x, min_x, max_x)
+            # 前のx座標更新
+            prev_x = pos_x
