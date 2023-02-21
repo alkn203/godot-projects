937cf5c1d5155331f53cd68caa310b37d383cf04
alkn203 <alkn203@gmail.com>
2023-02-21 15:08:56 +0900

    Update Enemy.gd

--- invader/Enemy.gd
+++ invader/Enemy.gd
@@ -1,30 +1,20 @@
+# 敵クラス
+class_name Enemy
 extends Area2D
 
-
-# Declare member variables here. Examples:
-var speed = 10
-var move_direction = -1
-var can_move_down = false
-const DOWN_DISTANCE = 32
+# 定数
 const PROB_BEAM = 0.0002
-onready var beam_scene = preload("res://Beam.tscn")
-onready var beam_layer = get_node("/root/Main/BeamLayer")
 
+# シーン
+const Beam = preload("res://Beam.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# ノード
+onready var beam_layer: CanvasLayer = get_node("/root/Main/BeamLayer")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x += move_direction * speed * delta
-	#
-	if can_move_down:
-		position.y += DOWN_DISTANCE
-		can_move_down = false
-	#
-	if randf() < PROB_BEAM:
-		var beam = beam_scene.instance()
-		beam.position = position
-		beam_layer.add_child(beam)
+# 毎フレーム処理
+func _process(delta: float) -> void:
+    # 一定の確率でビーム発射
+    if randf() < PROB_BEAM:
+        var beam: Beam = Beam.instance()
+	beam.position = position
+	beam_layer.add_child(beam)
