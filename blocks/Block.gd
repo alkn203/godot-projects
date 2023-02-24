99edbdb7fb005b40fc987903ccaa1d5cc59973d4
alkn203 <alkn203@gmail.com>
2023-02-24 22:23:20 +0900

    update

--- blocks/Block.gd
+++ blocks/Block.gd
@@ -1,4 +1,5 @@
 # Blockクラス
+class_name Block
 extends Node2D
 
 # 変数
@@ -6,3 +7,7 @@ var type: int = 0
 var drop_count: int = 0
 var index_pos: Vector2 = Vector2.ZERO
 var mark: String = "normal"
+
+# グレイスケール化
+func greyscale():
+    get_node("Sprite").material.set_shader_param("greyscale", true)
